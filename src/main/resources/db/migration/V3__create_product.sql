734c9ac1e172d41637f0eadd19fc9ad04144390b
redseaworld <redseaworld@yahoo.com>
2019-07-22 17:22:39 -0400

    Procuts, and Category model creation

---
+++ src/main/resources/db/migration/V3__create_product.sql
@@ -0,0 +1,21 @@
+
+
+CREATE TABLE products(
+    id SERIAL PRIMARY KEY NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    price NUMERIC(6,2) NOT NULL,
+    description TEXT NOT NULL
+);
+
+
+CREATE TABLE categories(
+    id SERIAL PRIMARY KEY NOT NULL,
+    name VARCHAR(255) NOT NULL
+
+);
+
+CREATE TABLE products_categories(
+    product_id INTEGER NOT NULL,
+    category_id INTEGER NOT NULL,
+    PRIMARY KEY(product_id, category_id)
+);
