03a6a409e72d567031acdae0c6d0c455599db108
Hieu Tran <hmtran.ae@gmail.com>
2019-07-23 12:00:14 -0400

    autowired repository in corresponding controller

--- src/main/java/com/launchacademy/programmingswaglist/controllers/ReviewsController.java
+++ src/main/java/com/launchacademy/programmingswaglist/controllers/ReviewsController.java
@@ -1,8 +1,16 @@
 package com.launchacademy.programmingswaglist.controllers;
 
+import com.launchacademy.programmingswaglist.repositories.ReviewRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class ReviewsController {
+  private ReviewRepository reviewRepository;
 
+  @Autowired
+  public ReviewsController(
+      ReviewRepository reviewRepository) {
+    this.reviewRepository = reviewRepository;
+  }
 }
