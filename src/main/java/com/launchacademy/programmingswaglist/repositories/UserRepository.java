fa361cb0e95b1b24b62d2ff357896c67a3a2cdfa
Katherine <manserka@oregonstate.edu>
2019-07-23 12:13:40 -0400

    paging and sorting

--- src/main/java/com/launchacademy/programmingswaglist/repositories/UserRepository.java
+++ src/main/java/com/launchacademy/programmingswaglist/repositories/UserRepository.java
@@ -2,9 +2,10 @@ package com.launchacademy.programmingswaglist.repositories;
 
 import com.launchacademy.programmingswaglist.models.User;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends CrudRepository<User, Integer> {
+public interface UserRepository extends PagingAndSortingRepository<User, Integer> {
   User findByUsername(String username);
 }
