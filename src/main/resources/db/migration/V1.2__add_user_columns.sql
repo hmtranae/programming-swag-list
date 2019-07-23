8ec0c606d4d982e7633308c8d9fdcc6352d98eed
Katherine <manserka@oregonstate.edu>
2019-07-23 10:16:16 -0400

    updated user schema foreign key with proper names to map to categories table

--- src/main/resources/db/migration/V1.2__add_user_columns.sql
+++ src/main/resources/db/migration/V1.2__add_user_columns.sql
@@ -1,4 +1,4 @@
 ALTER TABLE users
 ADD COLUMN first_name VARCHAR(255) NOT NULL,
 ADD COLUMN last_name VARCHAR(255) NOT NULL,
-ADD FOREIGN KEY (categories_id) REFERENCES categories(categories_id) NOT NULL;
+ADD FOREIGN KEY (category_id) REFERENCES categories(id) NOT NULL;
