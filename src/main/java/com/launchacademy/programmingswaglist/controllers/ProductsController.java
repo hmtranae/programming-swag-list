43c8e49414ee431195e0df44ff69b3fc797c88ec
redseaworld <redseaworld@yahoo.com>
2019-07-24 11:28:30 -0400

    created prodcut show and added index page

--- src/main/java/com/launchacademy/programmingswaglist/controllers/ProductsController.java
+++ src/main/java/com/launchacademy/programmingswaglist/controllers/ProductsController.java
@@ -3,6 +3,9 @@ package com.launchacademy.programmingswaglist.controllers;
 import com.launchacademy.programmingswaglist.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class ProductsController {
@@ -11,5 +14,17 @@ public class ProductsController {
 
     public ProductsController(ProductRepository productRepository) {
         this.productRepository = productRepository;
+    }
+
+    @GetMapping("/products/show/{productId}")
+    public String specificProduct(@PathVariable Integer productId, Model model){
+        productRepository.findById(productId).ifPresent(o -> model.addAttribute("product", o));
+        return "products/show";
+    }
+
+    @GetMapping("/products")
+    public String getProductList(Model model){
+        model.addAttribute("products", productRepository.findAll());
+        return "products/index";
     }
 }
