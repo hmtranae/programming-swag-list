36b1fae49f51e6da868a9d189358c0d517f96db1
Katherine <manserka@oregonstate.edu>
2019-07-24 13:40:59 -0400

    rest controller for products

--- src/main/java/com/launchacademy/programmingswaglist/controllers/ProductRestController.java
+++ src/main/java/com/launchacademy/programmingswaglist/controllers/ProductRestController.java
@@ -8,15 +8,20 @@ import com.launchacademy.programmingswaglist.repositories.RatingTypeRepository;
 import com.launchacademy.programmingswaglist.repositories.ReviewRepository;
 import com.launchacademy.programmingswaglist.repositories.RoleRepository;
 import com.launchacademy.programmingswaglist.repositories.UserRepository;
+import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ProductRestController {
-
   private final ProductRepository productRepository;
   private final CategoryRepository categoryRepository;
   private final RatingRepository ratingRepository;
@@ -24,6 +29,20 @@ public class ProductRestController {
   private final ReviewRepository reviewRepository;
   private final RoleRepository roleRepository;
   private final UserRepository userRepository;
+
+
+  @NoArgsConstructor
+  private class ProductNotFoundException extends RuntimeException {};
+
+  @ControllerAdvice
+  private class ProductNotFoundAdvice{
+    @ResponseBody
+    @ExceptionHandler(ProductNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    String ProductNotFoundHandler(ProductNotFoundException ex) {
+      return ex.getMessage();
+    }
+  }
 
   @Autowired
   public ProductRestController(ProductRepository productRepository, CategoryRepository categoryRepository, RatingRepository ratingRepository, RatingTypeRepository ratingTypeRepository, ReviewRepository reviewRepository, RoleRepository roleRepository, UserRepository userRepository) {
@@ -43,10 +62,14 @@ public class ProductRestController {
 
   @GetMapping("/api/v1/products/{productId}/show")
   public Product getProduct(@PathVariable Integer productId) {
-    return productRepository.findById(productId);
+    return productRepository.findById(productId).orElseThrow(() -> new ProductNotFoundException());
   }
-
 }
+
+
+
+
+
 
 
 
