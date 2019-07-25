f18e4eae6be475cceb695bd25072b47e17fa4430
Hieu Tran <hmtran.ae@gmail.com>
2019-07-25 10:39:37 -0400

    format product show files

--- src/main/frontend/app/components/ProductShowComponent.js
+++ src/main/frontend/app/components/ProductShowComponent.js
@@ -1,16 +1,15 @@
 import React from 'react';
-import { browserHistory, Link } from 'react-router';
 
 const ProductShowComponent = (props) => {
-  return(
+  return (
     <div className="product-show">
-        <h2>{props.name}</h2>
-        <h2>{props.price}</h2>
-        <p>{props.description}</p>
-        <h2>{props.url}</h2>
-        <img src = "{props.imageUrl}"/>
+      <h2>{props.name}</h2>
+      <h2>{props.price}</h2>
+      <p>{props.description}</p>
+      <h2>{props.url}</h2>
+      <img src="{props.imageUrl}" />
     </div>
-   )
- }
+  )
+}
 
 export default ProductShowComponent;
--- src/main/frontend/app/containers/ProductShowContainer.js
+++ src/main/frontend/app/containers/ProductShowContainer.js
@@ -4,32 +4,32 @@ import ProductShowComponent from '../components/ProductShowComponent';
 class ProductShowContainer extends Component {
   constructor(props) {
     super(props);
-        this.state = {
-          product: {}
-        }
-   }
+    this.state = {
+      product: {}
+    }
+  }
 
-   componentDidMount() {
+  componentDidMount() {
     let productId = this.props.params.id
-      fetch(`/api/v1/products/${productId}/show`)
-	      .then(response => response.json())
-        .then(body => {
-        this.setState({product: body})
-	     })
-	  }
+    fetch(`/api/v1/products/${productId}/show`)
+      .then(response => response.json())
+      .then(body => {
+        this.setState({ product: body })
+      })
+  }
 
   render() {
-    return(
+    return (
       <ProductShowComponent
-	        id={this.state.product.id}
-	        name={this.state.product.name}
-	        price={this.state.product.price}
-	        description={this.state.product.description}
-	        url={this.state.product.url}
-	        image={this.product.imageUrl}
-	      />
-	   )
-	  }
-	}
+        id={this.state.product.id}
+        name={this.state.product.name}
+        price={this.state.product.price}
+        description={this.state.product.description}
+        url={this.state.product.url}
+        image={this.product.imageUrl}
+      />
+    )
+  }
+}
 
 export default ProductShowContainer;
