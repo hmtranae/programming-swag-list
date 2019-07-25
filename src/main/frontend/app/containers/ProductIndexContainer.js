d77545dc39e02cf3c982e3cd0ccb60bf4b089690
redseaworld <redseaworld@yahoo.com>
2019-07-25 10:37:46 -0400

    implemented product index container

---
+++ src/main/frontend/app/containers/ProductIndexContainer.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+
+class ProductIndexContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: []
+    }
+
+    this.addNewProduct = this.addNewProduct.bind(this)
+  }
+
+  componentDidMount() {
+    fetch('/api/v1/products')
+      .then(response => {
+        return response.json()
+      })
+      .then(products => {
+        this.setState({ products: products.content })
+      })
+  }
+
+  render() {
+    let productList = this.state.products.map(product => {
+      return (
+        <div>
+          <li>Product Name: {product.name},  Price: {product.price},
+               url: {product.url}, Image_Url: {product.imageUrl}, Description: {product.description}</li>
+        </div>
+      )
+    })
+
+    return (
+      <div>
+        <h1>The Swaggest Product List!:</h1>
+        <ul>
+          {productList}
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default ProductIndexContainer;
