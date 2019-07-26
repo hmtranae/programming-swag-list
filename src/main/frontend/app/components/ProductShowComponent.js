a3fffcb0819bcfe35d88470b096ee542899c4d7f
redseaworld <redseaworld@yahoo.com>
2019-07-26 16:27:25 -0400

    w3c standards fix

--- src/main/frontend/app/components/ProductShowComponent.js
+++ src/main/frontend/app/components/ProductShowComponent.js
@@ -4,7 +4,7 @@ import '../css/showpage.scss'
 const ProductShowComponent = (props) => {
   return (
     <div className="flex-container">
-      <div><img src={props.image} /></div>
+      <div><img src={props.image} alt={props.name}/></div>
       <div>
         <div className="product-name">
           <h2>{props.name}</h2>
@@ -12,7 +12,11 @@ const ProductShowComponent = (props) => {
         <h2>Price: ${props.price} & Free Shippping</h2>
         <p><a href={props.url}>Visit Product Page</a></p>
         <div className="description">
-          <p>Product Description: <br />{props.description}</p>
+          <p>Product Description:</p>
+          <br/>
+          <ul>
+            {props.description}
+          </ul>
         </div>
       </div>
     </div>
