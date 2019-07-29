12ffdaadd1659ed5031617d042e65a9483854131
Hieu Tran <hmtran.ae@gmail.com>
2019-07-29 10:14:12 -0400

    show review form in product show page

--- src/main/frontend/app/components/ProductShowComponent.js
+++ src/main/frontend/app/components/ProductShowComponent.js
@@ -1,21 +1,25 @@
 import React from 'react';
-import '../css/showpage.scss'
+import '../css/showpage.scss';
+import ReviewForm from '../containers/ReviewForm';
 
 const ProductShowComponent = (props) => {
+  const { image, name, price, url, description } = props;
+
   return (
     <div className="flex-container">
-      <div><img src={props.image} alt={props.name} /></div>
+      <div><img src={image} alt={name} /></div>
       <div>
         <div className="product-name">
-          <h2>{props.name}</h2>
+          <h2>{name}</h2>
         </div>
-        <h2>Price: ${props.price} & Free Shippping</h2>
-        <p><a href={props.url}>Visit Product Page</a></p>
+        <h2>Price: ${price} & Free Shippping</h2>
+        <p><a href={url}>Visit Product Page</a></p>
         <div className="description">
           <p className="paragraph-spacing">Product Description:</p>
           <ul>
-            {props.description}
+            {description}
           </ul>
+          <ReviewForm />
         </div>
       </div>
     </div>
