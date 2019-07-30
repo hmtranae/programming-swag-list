b37eaddc813ff5c2e14b41b8b17f53d15c4b2679
Hieu Tran <hmtran.ae@gmail.com>
2019-07-30 14:12:03 -0400

    delete and edit review buttons

--- src/main/frontend/app/components/RatingTile.js
+++ src/main/frontend/app/components/RatingTile.js
@@ -7,6 +7,12 @@ const RatingTile = (props) => {
     <div>
       <p>Description: {description}</p>
       <p>Value: {value}</p>
+      <button type="button" className="btn btn-danger btn-xs">
+        <span className="glyphicon glyphicon-edit" /> Delete Review
+      </button>
+      <button type="button" className="btn btn-default btn-xs">
+        <span className="glyphicon glyphicon-edit" /> Edit Review
+      </button>
       <hr />
     </div>
   )
