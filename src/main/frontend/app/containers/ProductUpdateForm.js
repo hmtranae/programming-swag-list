2767e76f9bfe2715bebb44d7c44fa63f1b0fe9a8
Michael Kiflemariam <redseaworld@yahoo.com>
2019-07-30 11:01:12 -0400

    added product update form

---
+++ src/main/frontend/app/containers/ProductUpdateForm.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import FieldInput from '../components/FieldInput';
+import "../css/showpage.scss";
+
+class ProductUpdateForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = (
+      product: {
+        name: '',
+        price: ''
+    }
+   }
+
+
+   render(){
+   return()
+   }
+
+}
+
+export default ProductUpdateForm
--- src/main/java/com/launchacademy/programmingswaglist/controllers/ProductRestController.java
+++ src/main/java/com/launchacademy/programmingswaglist/controllers/ProductRestController.java
@@ -9,15 +9,7 @@ import com.launchacademy.programmingswaglist.repositories.UserRepository;
 import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class ProductRestController {
@@ -63,4 +55,12 @@ public class ProductRestController {
   public Product createProduct(@RequestBody Product product) {
     return productRepository.save(product);
   }
+
+  @PatchMapping("/api/v1/products/{productId}")
+  public Product productpdateProduct(@RequestBody Product partialUpdate, @PathVariable String id){
+   return productRepository.save(partialUpdate, id);
+  }
+
+
+
 }
