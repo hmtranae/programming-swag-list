9b980de863ea89e0d684f94e5c785051454c6d29
Hieu Tran <hmtran.ae@gmail.com>
2019-07-31 11:05:48 -0400

    differentiated between reviewsrestcontroller and reviewscontroller

--- src/main/java/com/launchacademy/programmingswaglist/controllers/ReviewsController.java
+++ src/main/java/com/launchacademy/programmingswaglist/controllers/ReviewsController.java
@@ -1,16 +1,13 @@
 package com.launchacademy.programmingswaglist.controllers;
 
-import com.launchacademy.programmingswaglist.controllers.ProductRestController.ProductNotFoundException;
-import com.launchacademy.programmingswaglist.models.Product;
-import com.launchacademy.programmingswaglist.models.Review;
 import com.launchacademy.programmingswaglist.repositories.ProductRepository;
 import com.launchacademy.programmingswaglist.repositories.ReviewRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
-import java.util.List;
-
-@RestController
+@Controller
 public class ReviewsController {
 
   private ReviewRepository reviewRepository;
@@ -23,21 +20,8 @@ public class ReviewsController {
     this.productRepository = productRepository;
   }
 
-  @PostMapping("/api/v1/reviews/{productId}")
-  public void createReview(@RequestBody Review review, @PathVariable Integer productId) {
-    Product product = productRepository.findById(productId)
-        .orElseThrow(() -> new ProductNotFoundException());
-    List<Review> reviews = product.getReviewsList();
-    review.setProduct(product);
-
-    reviews.add(review);
-    product.setReviewsList(reviews);
-
-    productRepository.save(product);
-  }
-
-  @GetMapping("/api/v1/reviews/{productId}")
-  public List<Review> getReviews(@PathVariable Integer productId) {
-    return reviewRepository.findAllByProductId(productId);
+  @GetMapping("/products/show/{productId}/reviews/{reviewId}")
+  public String getReviewForm(@PathVariable Integer productId, @PathVariable Integer reviewId) {
+    return "reviews/new";
   }
 }
---
+++ src/main/java/com/launchacademy/programmingswaglist/controllers/ReviewsRestController.java
@@ -0,0 +1,43 @@
+package com.launchacademy.programmingswaglist.controllers;
+
+import com.launchacademy.programmingswaglist.controllers.ProductRestController.ProductNotFoundException;
+import com.launchacademy.programmingswaglist.models.Product;
+import com.launchacademy.programmingswaglist.models.Review;
+import com.launchacademy.programmingswaglist.repositories.ProductRepository;
+import com.launchacademy.programmingswaglist.repositories.ReviewRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class ReviewsRestController {
+
+  private ReviewRepository reviewRepository;
+  private ProductRepository productRepository;
+
+  @Autowired
+  public ReviewsRestController(
+      ReviewRepository reviewRepository, ProductRepository productRepository) {
+    this.reviewRepository = reviewRepository;
+    this.productRepository = productRepository;
+  }
+
+  @PostMapping("/api/v1/reviews/{productId}")
+  public void createReview(@RequestBody Review review, @PathVariable Integer productId) {
+    Product product = productRepository.findById(productId)
+        .orElseThrow(() -> new ProductNotFoundException());
+    List<Review> reviews = product.getReviewsList();
+    review.setProduct(product);
+
+    reviews.add(review);
+    product.setReviewsList(reviews);
+
+    productRepository.save(product);
+  }
+
+  @GetMapping("/api/v1/reviews/{productId}")
+  public List<Review> getReviews(@PathVariable Integer productId) {
+    return reviewRepository.findAllByProductId(productId);
+  }
+}
