6f943d5b5c23e63ca9749c057369f512ed73c297
Hieu Tran <hmtran.ae@gmail.com>
2019-07-31 16:13:26 -0400

    fix product index merge

--- src/main/frontend/app/containers/ProductIndexContainer.js
+++ src/main/frontend/app/containers/ProductIndexContainer.js
@@ -9,7 +9,8 @@ class ProductIndexContainer extends Component {
       filtered: [],
       aggregateReviews: []
     }
-  this.handleChange = this.handleChange.bind(this);
+
+    this.handleChange = this.handleChange.bind(this);
   }
 
   componentDidMount() {
@@ -17,80 +18,64 @@ class ProductIndexContainer extends Component {
       .then(response => {
         return response.json()
       })
-      .then(products => {
+      .then(obj => {
         this.setState({
-        products: products,
-        filtered: products });
+          products: obj.products,
+          filtered: obj.products,
+          aggregateReviews: obj.aggregateReviews
+        });
       })
   }
 
   handleChange(e) {
-      let currentList = [];
-      let newList = [];
-      if (e.target.value !== "") {
-        currentList = this.state.filtered;
-        newList = currentList.filter(item => {
-          const lc = item.name.toLowerCase();
-          debugger;
-          const filter = e.target.value.toLowerCase();
-          return lc.includes(filter);
-        });
-      } else {
-        newList = this.state.products;
-      }
-      this.setState({
-        filtered: newList
+    let currentList = [];
+    let newList = [];
+    if (e.target.value !== "") {
+      currentList = this.state.filtered;
+      newList = currentList.filter(item => {
+        const lc = item.name.toLowerCase();
+        const filter = e.target.value.toLowerCase();
+        return lc.includes(filter);
       });
+    } else {
+      newList = this.state.products;
     }
-
-  render() {
-      const { filtered } = this.state;
-      let productList = filtered.map(product => {
-      return (
-        <figure className="col-md-4" key={product.id}>
-        <h2> <a href={`/products/show/${product.id}`}> {product.name} </a> </h2>
-        <h3>$ {product.price}</h3>
-        <a href={product.imageUrl}>
-          <img className="img-fluid" src={product.imageUrl} alt={product.name} />
-        </a>
-      .then(obj => {
-        this.setState({
-          products: obj.products,
-          aggregateReviews: obj.aggregateReviews
-        })
-      })
+    this.setState({
+      filtered: newList
+    });
   }
 
-
-
   render() {
-    const { products, aggregateReviews } = this.state;
-    let productList = products.map((product, i) => {
-      return (
-        <figure className="col-md-4" key={product.id}>
-          <h2> <a href={`/products/show/${product.id}`}> {product.name} </a> </h2>
-          <h3>$ {product.price}</h3>
-          <h3>Overalll Rating: {aggregateReviews[i].toFixed(2)}</h3>
-          <a href={product.imageUrl}>
-            <img className="img-fluid" src={product.imageUrl} alt={product.name} />
-          </a>
-        </figure>
+    const { filtered, aggregateReviews } = this.state;
+    if (filtered) {
+      let productList = filtered.map((product, i) => {
+        return (
+          <figure className="col-md-4" key={product.id}>
+            <h2> <a href={`/products/show/${product.id}`}> {product.name} </a> </h2>
+            <h3>$ {product.price}</h3>
+            <h3>Overall Rating: {aggregateReviews[i].toFixed(2)}</h3>
+            <a href={product.imageUrl}>
+              <img className="img-fluid" src={product.imageUrl} alt={product.name} />
+            </a>
+          </figure>
         )
-        })
+      })
 
-    return (
-      <div>
-      <div>
-         <input type="text" className="input" onChange={this.handleChange} placeholder="Search..." />
-       </div>
-        <a className='btn btn-primary float-right add-button' href="/products/new">Add a new Product</a>
-        <h1>All the Swaggest Programming Products</h1>
-        <hr />
-        <div className="row">
-          {productList}
+      return (
+        <div>
+          <input type="text" className="input" onChange={this.handleChange} placeholder="Search..." />
+          <h1>All the Swaggest Programming Products</h1>
+          <hr />
+          <div className="row">
+            {productList}
+          </div>
         </div>
-      </div>
-    )
+      )
+    } else {
+      return (
+        <div />
+      )
+    }
   }
 }
 
