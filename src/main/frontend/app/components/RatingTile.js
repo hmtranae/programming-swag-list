14e65f38de622ee4a570f4e2fd3910f6b1a74888
Hieu Tran <hmtran.ae@gmail.com>
2019-07-31 10:36:41 -0400

    clicking edit review will take to edit review form

--- src/main/frontend/app/components/RatingTile.js
+++ src/main/frontend/app/components/RatingTile.js
@@ -1,18 +1,19 @@
 import React from 'react';
 
 const RatingTile = (props) => {
-  const { value, description } = props;
+  const { id, value, description } = props;
+  let pathname = window.location.pathname;
 
   return (
     <div>
       <p>Description: {description}</p>
-      <p>Value: {value}</p>
-      <button type="button" className="btn btn-danger btn-xs">
-        <span className="glyphicon glyphicon-edit" /> Delete Review
+      <p>Rating: {value}</p>
+      <button type="button" className="btn btn-danger btn-sm">
+        Delete Review
       </button>
-      <button type="button" className="btn btn-default btn-xs">
-        <span className="glyphicon glyphicon-edit" /> Edit Review
-      </button>
+      <a href={`${pathname}/reviews/${id}`} className="btn btn-secondary btn-sm">
+        Edit Review
+      </a>
       <hr />
     </div>
   )
