1a480875b706537e6d4da869ea25f3bde0ccfe10
Hieu Tran <hmtran.ae@gmail.com>
2019-08-01 08:59:26 -0400

    styling for welcome page

--- src/main/frontend/app/components/Welcome.js
+++ src/main/frontend/app/components/Welcome.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import '../css/welcome.scss'
+import '../css/welcome.scss';
 
 const Welcome = (props) => {
 
   return (
     <div className="container">
       <h1>Check out some of the top dev must-haves!</h1>
-      <h2>What We Are All About</h2>
+      <h2 className="text-center">What We Are All About</h2>
       <p>Programmers of all levels want to know what sort of tech gear they need in order to work at peak efficiency.</p>
       <p>Dev Must Haves has already compiled the best list of gear according to what you value most:</p>
       <ul>
@@ -14,11 +14,9 @@ const Welcome = (props) => {
         <li>budget</li>
         <li>customizability</li>
       </ul>
-      <p>See what others have that you are missing in the tech world, leave reviews or suggest a new product for others to try!</p>
+      <p>See what you are missing in the tech world, leave reviews or suggest new products for others to try!</p>
 
-      <div><a href="/products"> Click here to view all products </a></div>
-      <div><a href="/registration"> Click here to register </a></div>
-      <div><a href="/login"> Click here to login </a></div>
+      <a className="container btn btn-primary btn-lg btn-block" href="/products"> Click here to view all products </a>
     </div>
   )
 }
